69bb02b738e086b224afeb5f41c4f37d66461fe7
shawncrisp <ypsirc@gmail.com>
2016-05-17 20:24:34 -0400

    Week-2-Day-1
    Added the camera button and image.

---
+++ Selfigram/ProfileViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ProfileViewController.swift
+//  Selfigram
+//
+//  Created by Shawn Crisp on 2016-05-17.
+//  Copyright © 2016 Shawn Crisp. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBAction func cameraButtonPressed(sender: AnyObject) {
+        
+        print("Camera Button Pressed!")
+        
+        
+        // 1: Create an ImagePickerController
+        let pickerController = UIImagePickerController()
+        
+        // 2: Self in this line refers to this View Controller
+        //    Setting the Delegate Property means you want to receive a message
+        //    from pickerController when a specific event is triggered.
+        pickerController.delegate = self
+        
+        if TARGET_OS_SIMULATOR == 1 {
+            // 3. We check if we are running on a Simulator
+            //    If so, we pick a photo from the simulator’s Photo Library
+            // We need to do this because the simulator does not have a camera
+            pickerController.sourceType = .PhotoLibrary
+        } else {
+            // 4. We check if we are running on an iPhone or iPad (ie: not a simulator)
+            //    If so, we open up the pickerController's Camera (Front Camera, for selfies!)
+            pickerController.sourceType = .Camera
+            pickerController.cameraDevice = .Front
+            pickerController.cameraCaptureMode = .Photo
+        }
+        
+        // Preset the pickerController on screen
+        self.presentViewController(pickerController, animated: true, completion: nil)
+        
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        usernameLabel.text = "Shawn"
+        
+        
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+   
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        // 1. When the delegate method is returned, it passes along a dictionary called info.
+        //    This dictionary contains multiple things that maybe useful to us.
+        //    We are getting an image from the UIImagePickerControllerOriginalImage key in that dictionary
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            //2. To our imageView, we set the image property to be the image the user has chosen
+            profileImageView.image = image
+            
+        }
+        
+        //3. We remember to dismiss the Image Picker from our screen.
+        dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
